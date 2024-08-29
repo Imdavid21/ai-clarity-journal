7303d4955926869498c1cb249876e783bbf161f6
Imdavid21 <84083101+Imdavid21@users.noreply.github.com>
2024-08-29 13:56:45 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "../styles/global.css";
 import { JournalProvider } from "./JournalContext.js";
-import { AuthProvider } from "./AuthContext.js";
 
 function MyApp({ Component, pageProps }) {
   return (
