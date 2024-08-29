15612059726498add9c98b18bb2abe95eb21ccb9
Imdavid21 <84083101+Imdavid21@users.noreply.github.com>
2024-08-29 14:56:47 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': './',
+    };
+    return config;
+  },
+}
 
 export default nextConfig;
