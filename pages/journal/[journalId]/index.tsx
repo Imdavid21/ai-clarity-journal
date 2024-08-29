a6ffddad1d8522c57c0efa479f20a2c54e95c388
Imdavid21 <84083101+Imdavid21@users.noreply.github.com>
2024-08-29 14:03:48 +0530

    Update index.tsx

--- pages/journal/[journalId]/index.tsx
+++ pages/journal/[journalId]/index.tsx
@@ -1,6 +1,5 @@
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
-import { useAuth } from "../../AuthContext.js";
 import axios from 'axios';
 import Header from "../../../components/common/Header";
 import TabComponent from "@/components/common/TabComponent";
